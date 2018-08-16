4ca0a4bfc78366b4f98fb3135c3337fa976e113d
ayuriyev <astalexxx@gmail.com>
2018-08-16 19:59:00 +0300

    markup

---
+++ js/main.js
@@ -0,0 +1,66 @@
+google.maps.event.addDomListener(window, "load", initMap);
+
+function init(){
+    $.getJSON("sample.json", function(result){
+        var data = [],
+            dataCollection = [];
+        for (var i = 0; i < result.length; i++){
+            data.push({
+                ip: result[i][0],
+                number: result[i][1],
+                latitude: '',
+                longitude: ''
+            })
+            if (data.length == 150){
+                dataCollection.push(data);
+                data = [];
+            }
+        };
+        if (data.length > 0){
+            dataCollection.push(data);
+        }
+        getCoordinates (dataCollection);
+    });
+}
+
+function getCoordinates (jsondata){
+    var counter = 0;
+    ipRequest(counter, jsondata);
+    counter++;
+    var timerId = setInterval(function() {
+        if (counter == jsondata.length){
+            clearInterval(timerId);
+        } else {
+            ipRequest(counter, jsondata);
+            counter++;
+        }
+    }, 60001);
+
+}
+
+function ipRequest(counter, jsondata){
+    for (var i = 0; i < jsondata[counter].length; i++){
+        let number = jsondata[counter][i].number;
+        $.getJSON("http://ip-api.com/json/"+jsondata[counter][i].ip, function(data) {
+            addMarker(data.lat, data.lon, number);
+        });
+    }
+}
+
+function initMap() {
+    var centerLatLng = new google.maps.LatLng(56.2928515, 43.7866641);
+    var mapOptions = {
+        center: centerLatLng,
+        zoom: 3
+    };
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    init();
+}
+
+function addMarker(lat, lon, number){
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lon),
+        map: map,
+        title: "Requests: " + number
+    });
+}
